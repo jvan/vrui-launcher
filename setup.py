c8559f3251fd5de45b21c4b9f5bc8edcef1ffef1
jvan <jvan@jvanlabs.net>
2013-05-10 11:55:07 -0700

    Replaced install script with distutils installer.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import os
+
+list_files = lambda d: [os.path.join(d, f) for f in os.listdir(d)]
+
+template_files = list_files('templates')
+fragment_files = list_files('fragments')
+
+setup(
+      name = 'vrui launcher',
+      version = '0.0.1',
+      author = 'Jordan Van Aalsburg',
+      author_email = 'jvan@cse.ucdavis.edu',
+      url = 'http://iviz.csc.ucdavis.edu',
+      description = 'Launcher utility for VRUI applications',
+      packages = [ 'vruilauncher', 'vruilauncher.widgets' ],
+      scripts = [ 'vrui-launcher', 'vrui-launcher-settings' ],
+      data_files = [ 
+               ('/usr/local/share/vrui-launcher/templates', template_files),
+               ('/usr/local/share/vrui-launcher/fragments', fragment_files),
+               ('/usr/share/glib-2.0/schemas', ['etc/apps.vrui-launcher.gschema.xml'])
+                   ]
+)
+                     
